a765b556fc125168c94ae82af00d719037ba35d7
Michael3682 <152974233+Michael3682@users.noreply.github.com>
2024-02-09 01:19:21 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const cube = document.getElementById('cube')
+const button = document.getElementById('btn')
 const piece1 = document.getElementById('piece1')
 const piece2 = document.getElementById('piece2')
 const piece3 = document.getElementById('piece3')
@@ -7,23 +8,27 @@ const piece5 = document.getElementById('piece5')
 const piece6 = document.getElementById('piece6')
 const piece7 = document.getElementById('piece7')
 
-cube.addEventListener('mouseover', () => {
-    cube.classList.add('op')
-    piece1.classList.add('op1')
-    piece2.classList.add('op2')
-    piece3.classList.add('op3')
-    piece4.classList.add('op4')
-    piece5.classList.add('op5')
-    piece6.classList.add('op6')
-    piece7.classList.add('op7')
-})
-cube.addEventListener('mouseout', () => {
-    cube.classList.remove('op')
-    piece1.classList.remove('op1')
-    piece2.classList.remove('op2')
-    piece3.classList.remove('op3')
-    piece4.classList.remove('op4')
-    piece5.classList.remove('op5')
-    piece6.classList.remove('op6')
-    piece7.classList.remove('op7')
+button.addEventListener('click', () => {
+    if (button.innerText == "Open Cube") {
+        button.innerText = "Close Cube"
+        cube.classList.add('op')
+        piece1.classList.add('op1')
+        piece2.classList.add('op2')
+        piece3.classList.add('op3')
+        piece4.classList.add('op4')
+        piece5.classList.add('op5')
+        piece6.classList.add('op6')
+        piece7.classList.add('op7')
+    }
+    else if (button.innerText == "Close Cube") {
+        button.innerText = "Open Cube"
+        cube.classList.remove('op')
+        piece1.classList.remove('op1')
+        piece2.classList.remove('op2')
+        piece3.classList.remove('op3')
+        piece4.classList.remove('op4')
+        piece5.classList.remove('op5')
+        piece6.classList.remove('op6')
+        piece7.classList.remove('op7')
+    }
 })
